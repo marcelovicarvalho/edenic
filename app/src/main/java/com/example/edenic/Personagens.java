c2ccabcb0916e7400bc23a044f6d4f215fadfc69
Marcelo <marcelo7v7carvaho@gmail.com>
2019-11-23 15:26:00 -0300

    nao envia pro banco mas ta puxando os char que ja tem no banco

--- app/src/main/java/com/example/edenic/Personagens.java
+++ app/src/main/java/com/example/edenic/Personagens.java
@@ -1,8 +1,12 @@
 package com.example.edenic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,13 +14,23 @@ import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class Personagens extends AppCompatActivity {
+
+    final ArrayList<String> nome = new ArrayList<>();
+    final ArrayList<String> nivel = new ArrayList<>();
+    final ArrayList<String> classe = new ArrayList<>();
+    final ArrayList<String> raca= new ArrayList<>();
+    final ArrayList<String> background = new ArrayList<>();
+
+    RecyclerView rv2;
+    RecyclerView.LayoutManager mRv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personagens);
-
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -28,11 +42,34 @@ public class Personagens extends AppCompatActivity {
                 AddPersonagem();
             }
         });
+
+        rv2 = findViewById(R.id.recyclerviewPersonagens);
+
+        setAdapter();
     }
 
     public void AddPersonagem() {
         Intent intent = new Intent(Personagens.this, AddPersonagem.class);
         startActivity(intent);
+    }
+
+    public void setAdapter(){
+        DatabaseHandler banco = new DatabaseHandler(this);
+        SQLiteDatabase db = banco.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from personagens", new String[]{});
+
+        cursor.moveToFirst();
+        do {
+            nome.add(cursor.getString(1));
+            nivel.add(cursor.getString(2));
+            classe.add(cursor.getString(4));
+            raca.add(cursor.getString(3));
+            background.add(cursor.getString(5));
+        } while (cursor.moveToNext());
+        PersonagensRecycler adapter = new PersonagensRecycler(this, nome, nivel, classe, raca, background);
+        rv2.setAdapter(adapter);
+        mRv2 = new GridLayoutManager(this.getBaseContext(), 1);
+        rv2.setLayoutManager(mRv2);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
---
+++ app/src/main/java/com/example/edenic/PersonagensRecycler.java
@@ -0,0 +1,86 @@
+package com.example.edenic;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PersonagensRecycler extends RecyclerView.Adapter<PersonagensRecycler.MyViewHolder> {
+
+    ArrayList<String> nome = new ArrayList<>();
+    ArrayList<String> nivel = new ArrayList<>();
+    ArrayList<String> classe = new ArrayList<>();
+    ArrayList<String> raca = new ArrayList<>();
+    ArrayList<String> background = new ArrayList<>();
+    Context context;
+
+    public PersonagensRecycler(Context context, ArrayList<String> nome, ArrayList<String> nivel, ArrayList<String> classe, ArrayList<String> raca, ArrayList<String> background) {
+        context = context;
+        this.nome = nome;
+        this.nivel = nivel;
+        this.classe = classe;
+        this.raca = raca;
+        this.background = background;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        LinearLayout txt = (LinearLayout) LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_personagens, viewGroup, false);
+        PersonagensRecycler.MyViewHolder myViewHolder = new PersonagensRecycler.MyViewHolder(txt);
+        Collections.sort(nome);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder Holder, int i) {
+        Holder.nome.setText(nome.get(i));
+        Holder.nivel.setText(nivel.get(i));
+        Holder.classe.setText(classe.get(i));
+        Holder.raca.setText(raca.get(i));
+        Holder.background.setText(background.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return nome.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView nome;
+        TextView nivel;
+        TextView classe;
+        TextView raca;
+        TextView background;
+        CardView cardView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nome = itemView.findViewById(R.id.nome);
+            nivel = itemView.findViewById(R.id.nivel);
+            classe = itemView.findViewById(R.id.classe);
+            raca = itemView.findViewById(R.id.raca);
+            background = itemView.findViewById(R.id.background);
+            cardView = itemView.findViewById(R.id.cardV2);
+        }
+    }
+
+}
